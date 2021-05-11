c5e2d0e9754361c3cc1e3d9f94e6533fd79e2c63
Filippe da Silva <filippe.silva@bradesco.com.br>
2021-05-11 19:25:14 -0300

    removendo gerenciamento do estado da aplicação do app.js

--- src/App.js
+++ src/App.js
@@ -5,51 +5,56 @@ import './assets/index.css';
 import './assets/App.css'
 import { nativeTouchData } from 'react-dom/test-utils';
 import { ListaDeCategorias } from './components/ListaDeCategorias';
+import Categorias from './dados/Categorias';
+import ArrayDeNotas from './dados/ArrayDeNotas';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      notas: [],
-      categorias: []
-    }
+    // this.state = {
+    //   notas: [],
+    //   // categorias: []
+    // }
+
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
 
   }
 
-  criarNota(titulo, texto , categoria) {
-    const novaNota = { titulo, texto, categoria };
-    const novoEstado = [...this.state.notas, novaNota];
-    this.setState({ notas: novoEstado });
-  }
+  // criarNota(titulo, texto , categoria) {
+  //   const novaNota = { titulo, texto, categoria };
+  //   const novoEstado = [...this.state.notas, novaNota];
+  //   this.setState({ notas: novoEstado });
+  // }
 
-  deletarNota(indice) {
-    let notas = this.state.notas;
-    notas.splice(indice, 1);
-    this.setState({ notas: notas });
+  // deletarNota(indice) {
+  //   let notas = this.state.notas;
+  //   notas.splice(indice, 1);
+  //   this.setState({ notas: notas });
 
-  }
+  // }
 
-  adicionarCategoria(categoria) {
-    this.setState({
-      categorias: [...this.state.categorias, categoria]
-    })
-  }
+  // adicionarCategoria(categoria) {
+  //   this.setState({
+  //     categorias: [...this.state.categorias, categoria]
+  //   })
+  // }
 
   render() {
     return (
       <section className='conteudo'>
         <FormularioCadastro
-          categorias={this.state.categorias}
-          criarNota={this.criarNota.bind(this)}
+          categorias={this.categorias.categorias}
+          criarNota={this.notas.adicionarNota}
         />
         <main className='conteudo-principal'>
           <ListaDeCategorias
-            categorias={this.state.categorias}
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
+            categorias={this.categorias.categorias}
+            adicionarCategoria={this.categorias.adicionarCategoria}
           />
           <ListaDeNotas
-            apagarNota={this.deletarNota.bind(this)}
-            notas={this.state.notas}
+            apagarNota={this.notas.apagarNota}
+            notas={this.notas.notas}
           />
         </main>
       </section>
